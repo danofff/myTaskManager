78a39afbcc140d9610aa9ce912f31071410dbda5
dan <danofff@mail.ru>
2018-02-26 16:22:21 +0600

    Добавьте файлы проекта.

---
+++ sys_prog_hw1/MyProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace sys_prog_hw1
+{
+    public class MyProcess
+    {    
+        public string NameProcess { get; }
+        public int IdProcess { get;}
+        public List<ProcessThread> Threads { get; }
+    }
+}
